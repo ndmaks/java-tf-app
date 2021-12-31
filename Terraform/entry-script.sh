88b60c6ec0a45d02b39e7a9267b703a0c459830e
ndmaks <nedimaksu4@gmail.com>
2021-12-31 22:57:49 +0300

    jenkinsfile added

--- Terraform/entry-script.sh
+++ Terraform/entry-script.sh
@@ -3,10 +3,11 @@
 sudo yum update -y && sudo yum install -y 
 #sudo service docker start
 sudo systemctl start docker 
-sudo groupadd docker
+#sudo groupadd docker
 sudo usermod -aG docker ec2-user
+sudo usermod -a -G docker jenkins
 #sudo reboot
-chmod 777 /var/run/docker.sock
+#chmod 777 /var/run/docker.sock
 
 # install docker-compose 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
