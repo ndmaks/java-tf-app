6b42d78484ce7c78cb33f5e3db222a92bd74e934
ndmaks <nedimaksu4@gmail.com>
2022-01-03 22:02:30 +0300

    github added

--- Terraform/entry-script.sh
+++ Terraform/entry-script.sh
@@ -8,8 +8,8 @@ sudo systemctl start docker
 #sudo reboot
 #sudo groupadd docker
 sudo usermod -aG docker ec2-user
-newgrp docker
-#chmod 666 /var/run/docker.sock
+#newgrp docker
+chmod 666 /var/run/docker.sock
 # install docker-compose 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
